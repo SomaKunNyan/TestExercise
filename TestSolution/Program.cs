9402ff22698c16738b2e236b73d2301e5f4861d6
LexyNyan <ifoggyelf@gmail.com>
2022-06-30 13:07:01 +0600

    optimization fix

--- TestSolution/Program.cs
+++ TestSolution/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace TestSolution
 {
@@ -6,14 +7,18 @@ namespace TestSolution
     {
         static void Main(string[] args)
         {
+            Stopwatch stopwatch = new Stopwatch();
             int elementsCount = 2;
             Console.WriteLine("Write count of elements of array ");
             elementsCount = Int32.Parse(Console.ReadLine());
             int[] filledArray = FillArray(elementsCount);
             SumOfMaxArrValues SumOfElements = new SumOfMaxArrValues(filledArray);
             MultiplicationOfMaxArrValues MultOfElements = new MultiplicationOfMaxArrValues(filledArray);
+            stopwatch.Start();
             Console.WriteLine("Summ of 2 max elements in array = "+SumOfElements.GetResult());
             Console.WriteLine("Multiplication of 2 max elements in array = "+MultOfElements.GetResult());
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
         }
 
         static int[] FillArray(int lenght)
@@ -39,9 +44,16 @@ namespace TestSolution
         
         public int GetResult()
         {
-            int[] sortedArr = values;
-            Array.Sort(sortedArr);
-            return sortedArr[sortedArr.Length - 1] + sortedArr[sortedArr.Length - 2];
+            int[] maxValues = new []{values[0],values[0]};
+            for (int i = 0; i < values.Length-1; i++)
+            {
+                if (values[i] > maxValues[0])
+                {
+                    maxValues[0] = values[i];
+                }
+                else if (values[i] > maxValues[1]) maxValues[1] = values[i];
+            }
+            return maxValues[0] + maxValues[1];
         }
     }
     
@@ -55,9 +67,16 @@ namespace TestSolution
 
         public int GetResult()
         {
-            int[] sortedArr = values;
-            Array.Sort(sortedArr);
-            return values[sortedArr.Length - 1] * values[sortedArr.Length - 2];
+            int[] maxValues = new []{values[0],values[0]};
+            for (int i = 0; i < values.Length-1; i++)
+            {
+                if (values[i] > maxValues[0])
+                {
+                    maxValues[0] = values[i];
+                }
+                else if (values[i] > maxValues[1]) maxValues[1] = values[i];
+            }
+            return maxValues[0] * maxValues[1];
         }
     }
 }
